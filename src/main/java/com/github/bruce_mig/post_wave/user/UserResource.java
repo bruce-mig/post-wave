46d1dc35879071dd94d19e8c9f11c6a387f37439
bruce-mig <brucetmig@icloud.com>
2024-06-22 22:54:09 +0200

    added delete and create user APIs

--- src/main/java/com/github/bruce_mig/post_wave/user/UserResource.java
+++ src/main/java/com/github/bruce_mig/post_wave/user/UserResource.java
@@ -1,9 +1,11 @@
 package com.github.bruce_mig.post_wave.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,6 +23,26 @@ public class UserResource {
 
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable Integer id){
-        return service.findOne(id);
+        User user = service.findOne(id);
+        if(user==null){
+            throw new UserNotFoundException("User not found with id:"+ id);
+        }
+
+        return user;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user){
+        User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable Integer id){
+        service.deleteById(id);
     }
 }
