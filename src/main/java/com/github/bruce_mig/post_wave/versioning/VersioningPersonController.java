b4bc9d646165f4094dbb6eed941942d51c9d822a
bruce-mig <brucetmig@icloud.com>
2024-06-23 18:52:48 +0200

    Multiple versioning types

--- src/main/java/com/github/bruce_mig/post_wave/versioning/VersioningPersonController.java
+++ src/main/java/com/github/bruce_mig/post_wave/versioning/VersioningPersonController.java
@@ -15,4 +15,41 @@ public class VersioningPersonController {
     public PersonV2 getPersonV2() {
         return new PersonV2(new Name("Bob","Charlie"));
     }
+
+    // Request Parameter versioning - Amazon
+
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getPersonRequestParameterV1() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getPersonRequestParameterV2() {
+        return new PersonV2(new Name("Bob","Charlie"));
+    }
+
+    // Request Header versioning - Microsoft
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 getPersonRequestHeaderV1() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 getPersonRequestHeaderV2() {
+        return new PersonV2(new Name("Bob","Charlie"));
+    }
+
+
+    // Content Negotiation Versioning  - GitHub
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getPersonAcceptHeaderV1() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getPersonAcceptHeaderV2() {
+        return new PersonV2(new Name("Bob","Charlie"));
+    }
 }
