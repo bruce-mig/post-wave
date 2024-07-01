cd4f9f5dd54d26a279aff7921f1b251a90e12171
bruce-mig <brucetmig@icloud.com>
2024-07-01 09:35:03 +0200

    added CORS mapping to allow front end origin

--- src/main/java/com/github/bruce_mig/post_wave/PostWaveApplication.java
+++ src/main/java/com/github/bruce_mig/post_wave/PostWaveApplication.java
@@ -2,6 +2,9 @@ package com.github.bruce_mig.post_wave;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PostWaveApplication {
@@ -10,4 +13,14 @@ public class PostWaveApplication {
 		SpringApplication.run(PostWaveApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer  corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:3000/");
+			}
+		};
+	}
 }
