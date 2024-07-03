4de32f2c07eb7d14cbbef812ea98b0873499842a
bruce-mig <brucetmig@icloud.com>
2024-07-03 15:35:48 +0200

    todo repository

---
+++ src/main/java/com/github/bruce_mig/post_wave/todo/TodoRepository.java
@@ -0,0 +1,9 @@
+package com.github.bruce_mig.post_wave.todo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TodoRepository extends JpaRepository<Todo, Integer> {
+    List<Todo> findByUsername(String username);
+}
