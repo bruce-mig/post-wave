b76ce8fd5868b6a97ce7738a4c27497efa095a92
bruce-mig <brucetmig@icloud.com>
2024-07-03 08:11:41 +0200

    added jwt package

---
+++ src/main/java/com/github/bruce_mig/post_wave/jwt/JwtTokenRequest.java
@@ -0,0 +1,3 @@
+package com.github.bruce_mig.post_wave.jwt;
+
+public record JwtTokenRequest(String username, String password) {}
---
+++ src/main/java/com/github/bruce_mig/post_wave/jwt/JwtTokenResponse.java
@@ -0,0 +1,3 @@
+package com.github.bruce_mig.post_wave.jwt;
+
+public record JwtTokenResponse(String token) {}
