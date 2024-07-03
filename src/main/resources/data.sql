cff1dd8267daa48ed59bf42ed22ec974b4c81301
bruce-mig <brucetmig@icloud.com>
2024-07-03 15:35:38 +0200

    init sql script for h2 db

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo (id, description, done, target_date, username)
+values (10001, 'Get AWS SA certified', false, CURRENT_DATE(), 'name');
+
+insert into todo (id, description, done, target_date, username)
+values (10002, 'Learn Ansible', false, CURRENT_DATE(), 'name');
+
+insert into todo (id, description, done, target_date, username)
+values (10003, 'Learn C', false, CURRENT_DATE(), 'name');
