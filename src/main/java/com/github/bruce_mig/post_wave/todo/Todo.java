4610de2837a29f4af4b8316e7782b221c37d64ef
bruce-mig <brucetmig@icloud.com>
2024-07-03 15:35:16 +0200

    modified from record to class

--- src/main/java/com/github/bruce_mig/post_wave/todo/Todo.java
+++ src/main/java/com/github/bruce_mig/post_wave/todo/Todo.java
@@ -1,12 +1,24 @@
 package com.github.bruce_mig.post_wave.todo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import lombok.*;
 import java.time.LocalDate;
 
-public record Todo(
-        int id,
-        String username,
-        String description,
-        LocalDate targetDate,
-        boolean done
-) {
+@Entity
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Todo {
+        @Id
+        @GeneratedValue
+        private Integer id;
+        private String username;
+        private String description;
+        private LocalDate targetDate;
+        private boolean done;
 }
